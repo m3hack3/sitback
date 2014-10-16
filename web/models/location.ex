681a32faaca7d18465165548a3f398033ade1650
Takayuki Matsubara <takayuki.1229@gmail.com>
2014-10-16 23:54:23 +0900

    remove IO.puts...

--- web/models/location.ex
+++ web/models/location.ex
@@ -4,9 +4,6 @@ defmodule Sitback.Location do
       "1_0" => "自席",
       "1_1" => "トイレ"
     }
-    IO.puts major
-    IO.puts minor
-    IO.puts "#{major}_#{minor}"
     locations["#{major}_#{minor}"]
   end
 end 
